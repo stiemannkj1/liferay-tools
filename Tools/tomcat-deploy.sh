9bbdf26c01c339851c44aa169885cb187575e755
Kyle Stiemann <stiemannkj1@gmail.com>
2015-02-02 16:46:06 -0500

    ~/Tools/tomcat-deploy.sh: Fixed paths. Added wildcards to commands so that they do not break when versions change.

--- Tools/tomcat-deploy.sh
+++ Tools/tomcat-deploy.sh
@@ -1,14 +1,12 @@
 #!/opt/local/bin/bash
 
-LAST_RUN_TIME_STAMP_FILE=$HOME/Tools/my-tools/.tomcat-deploy.sh.lastrun
+LAST_RUN_TIME_STAMP_FILE=$HOME/Tools/.tomcat-deploy.sh.lastrun
 TOMCAT_WEBAPPS=$HOME/Servers/apache-tomcat-7.0.56/webapps
-TOMCAT_SHOWCASE=$TOMCAT_WEBAPPS/showcase-webapp-4.2.0-beta1-SNAPSHOT
 
 echo "Removing showcase files from Tomcat."
-rm -rf "$TOMCAT_SHOWCASE/"
-rm -rf "$TOMCAT_SHOWCASE.war"
+rm -rf "$TOMCAT_WEBAPPS"/showcase-webapp-4.2.0-*
 
-LIFERAY_FACES_MASTER=$HOME/Projects/liferay.com/liferay-faces-master
+LIFERAY_FACES_MASTER=$HOME/Projects/liferay.com/faces/master
 
 if [[ "$@" == *u* || $(find "$LIFERAY_FACES_MASTER/util" -mnewer "$LAST_RUN_TIME_STAMP_FILE" |  head -n 1) ]]
 then
@@ -25,7 +23,7 @@ SHOWCASE_WEBAPP=$LIFERAY_FACES_MASTER/demos/showcase/showcase-webapp
 cd "$SHOWCASE_WEBAPP"
 mvn install -P development
 echo "Copying showcase.war to Tomcat."
-cp "$SHOWCASE_WEBAPP/target/showcase-webapp-4.2.0-beta1-SNAPSHOT.war" "$TOMCAT_WEBAPPS/"
+cp "$SHOWCASE_WEBAPP"/target/showcase-webapp-*-SNAPSHOT.war "$TOMCAT_WEBAPPS/"
 echo "Done."
 
 touch "$LAST_RUN_TIME_STAMP_FILE"
