e33163b4d3232032dcc4383e4c5eccc9462a53a2
Kyle Stiemann <stiemannkj1@gmail.com>
2015-02-11 16:31:10 -0500

    Added more detailed grep regex to liferay-tail.sh.

--- Tools/liferay-tail.sh
+++ Tools/liferay-tail.sh
@@ -1,8 +1,8 @@
 #!/opt/local/bin/bash
 
-tail -n 0 -f ../logs/catalina.out | tee /dev/tty | grep -q "INFO: Server startup" && growl.scpt "Liferay Started."
+tail -n 0 -f ../logs/catalina.out | tee /dev/tty | grep -q "INFO: Server startup in " && growl.scpt "Liferay Started."
 
 while true; do
-	tail -n 0 -f ../logs/catalina.out | tee /dev/tty | grep -q ".* is available for use" && growl.scpt "Portlet Available for Use."
+	tail -n 0 -f ../logs/catalina.out | tee /dev/tty | grep -q " for .* is available for use" && growl.scpt "Portlet Available for Use."
 done
 
