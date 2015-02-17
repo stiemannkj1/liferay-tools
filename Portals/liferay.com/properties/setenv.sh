432ccbe996489e4f8894d01387d89238526ed4e7
Kyle Stiemann <stiemannkj1@gmail.com>
2015-02-17 14:55:56 -0500

    Changed setenv.sh to add more memory to the JVM and enable jrebel. Also added comment which explains how to create a symlink for setenv.sh in all liferay/tomcat/bin folders.

--- Portals/liferay.com/properties/setenv.sh
+++ Portals/liferay.com/properties/setenv.sh
@@ -1,1 +1,3 @@
-JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF8 -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=256m"
+# This file is symlinked in every portal/tomcat/bin folder using the following command:
+# for folder in $(find . -name "tomcat-*" -type d); do (cd $folder/bin/ && ln -s /Users/kylestiemann/Portals/liferay.com/properties/setenv.sh setenv.sh); done
+JAVA_OPTS="$JAVA_OPTS -javaagent:/Users/kylestiemann/.jrebel/jrebel.jar -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true  -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx2048m -XX:MaxPermSize=512m"
