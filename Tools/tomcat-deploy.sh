5a17d21a87d6c3d497badc8195db35a4925bcf02
Kyle Stiemann <stiemannkj1@gmail.com>
2015-02-27 17:25:38 -0500

    Added tomcat-deploy.sh file to deploy webapps to tomcat.

---
+++ Tools/tomcat-deploy.sh
@@ -0,0 +1,17 @@
+#!/opt/local/bin/bash
+
+TOMCAT_WEBAPPS=/Users/kylestiemann/Servers/apache-tomcat-7.0.56/webapps
+
+echo "Removing $TOMCAT_WEBAPPS/${PWD##*/}* ..."
+rm -rf $TOMCAT_WEBAPPS/${PWD##*/}*
+echo "Done."
+
+echo "Building..."
+mvn clean install
+echo "Done."
+
+echo "Copying" target/*.war "to $TOMCAT_WEBAPPS/ ..."
+cp target/*.war $TOMCAT_WEBAPPS/
+echo "Done."
+
+growl.scpt "Process Completed."
