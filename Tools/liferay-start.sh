d9f9e98d0216053d3fc3a00807ea501163d739bb
Kyle Stiemann <stiemannkj1@gmail.com>
2015-03-04 14:42:56 -0500

    Added comment to liferay-start.sh explaining the location and status of jrebel.jar

--- Tools/liferay-start.sh
+++ Tools/liferay-start.sh
@@ -1,6 +1,8 @@
 #!/opt/local/bin/bash
 
 if [ -n "$1" ] && [[ "$1" == "jrebel" ]]; then
+	# jrebel.jar is symlinked to the NetBeans version of jrebel.jar via the following command:
+	# ln -s "/Applications/NetBeans/NetBeans 8.0.app/Contents/Resources/NetBeans/java2/jrebel/jrebel.jar" .jrebel/jrebel.jar
 	export JAVA_OPTS="$JAVA_OPTS -javaagent:/Users/kylestiemann/.jrebel/jrebel.jar"
 	./catalina.sh start
 elif [ -n "$1" ] && [[ "$1" == "debug" ]]; then
