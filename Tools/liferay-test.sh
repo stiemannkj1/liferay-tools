352067cd0329aedf573f3f79f17472d512ad7455
Kyle Stiemann <stiemannkj1@gmail.com>
2015-05-11 09:02:15 -0400

    In liferay-test.sh, improved find command by switching to gfind, specifying "-maxdepth 2", and checking for resources tests.

--- Tools/liferay-test.sh
+++ Tools/liferay-test.sh
@@ -2,6 +2,6 @@
 
 LIFERAY_FACES_TEST_INTEGRATION="$LIFERAY_FACES/test/integration"
 
-for portlet in $(find "$LIFERAY_FACES_TEST_INTEGRATION/demos" "$LIFERAY_FACES_TEST_INTEGRATION/issues" -name *-portlet | egrep $1); do
-	cd $portlet && mvn test -P integration
+for test in $(gfind "$LIFERAY_FACES_TEST_INTEGRATION/demos" "$LIFERAY_FACES_TEST_INTEGRATION/issues" -maxdepth 2 -regex ".*\(-portlet\|-resources\)" | egrep "$1"); do
+	cd $test && mvn test -P integration
 done
