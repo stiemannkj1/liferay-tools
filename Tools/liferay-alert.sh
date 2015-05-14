fe8e66750a89bcafe9768eb56e2b39a56739a4ee
Kyle Stiemann <stiemannkj1@gmail.com>
2015-05-14 08:40:32 -0400

    In liferay-alert.sh, made the command for checking if liferay has started more specific.

--- Tools/liferay-alert.sh
+++ Tools/liferay-alert.sh
@@ -2,7 +2,7 @@
 
 CATALINA_OUT=$(find . -name "tomcat-*" | head -1)/logs/catalina.out
 
-tail -n 0 -f $CATALINA_OUT | tee /dev/tty | grep -q "INFO: Server startup in " && growl.scpt "Liferay Started."
+tail -n 0 -f $CATALINA_OUT | tee /dev/tty | grep -q "org[.]apache[.]catalina[.]startup[.]Catalina start" && growl.scpt "Liferay Started."
 
 while true; do
 	tail -n 0 -f $CATALINA_OUT | tee /dev/tty | grep -q " for .* is available for use" && growl.scpt "Portlet Available for Use."
