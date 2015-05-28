bf7db8571b5867cbfad9827b30199fd0f9653231
Kyle Stiemann <stiemannkj1@gmail.com>
2015-05-28 11:56:44 -0400

    In liferay-deploy.sh, added development profile to deploy.sh command.

--- Tools/liferay-deploy.sh
+++ Tools/liferay-deploy.sh
@@ -8,7 +8,7 @@ for portlet in $(gfind "$LIFERAY_FACES/demos" "$LIFERAY_FACES/issues" -maxdepth 
 	if [[ $portlet =~ icefaces3-chat-portlet ]]; then
 		echo "Skipping $portlet."
 	elif [ -e "$portlet/pom.xml" ]; then
-		cd $portlet && deploy.sh "${@:2}"
+		cd $portlet && deploy.sh "${@:2}" development
 		mv ~/Portals/liferay.com/liferay-portal-$LIFERAY_VERSION-jsf-$JSF_VERSION/deploy/${portlet##*/}* ~/Portals/liferay.com/${LIFERAY_VERSION:0:3}/deploy/
 		echo "Moving ~/Portals/liferay.com/liferay-portal-$LIFERAY_VERSION-jsf-$JSF_VERSION/deploy/${portlet##*/}* to ~/Portals/liferay.com/${LIFERAY_VERSION:0:3}/deploy/"
 	else
@@ -18,14 +18,14 @@ done
 
 if [[ tck =~ $1 ]]; then
 
-	cd $LIFERAY_FACES/test/bridge-tck/bridge-tck-main-portlet && deploy.sh "${@:2}"
+	cd $LIFERAY_FACES/test/bridge-tck/bridge-tck-main-portlet && deploy.sh "${@:2}" development
 	mv ~/Portals/liferay.com/liferay-portal-$LIFERAY_VERSION-jsf-$JSF_VERSION/deploy/bridge-tck-main-portlet* ~/Portals/liferay.com/${LIFERAY_VERSION:0:3}/deploy/
 	echo "Moving ~/Portals/liferay.com/liferay-portal-$LIFERAY_VERSION-jsf-$JSF_VERSION/bridge-tck-main-portlet* to ~/Portals/liferay.com/${LIFERAY_VERSION:0:3}/deploy/"
 fi
 
 if [[ test-setup-hook =~ $1 ]]; then
 
-	cd $LIFERAY_FACES/test/test-setup-hook && deploy.sh "${@:2}"
+	cd $LIFERAY_FACES/test/test-setup-hook && deploy.sh "${@:2}" development
 	mv ~/Portals/liferay.com/liferay-portal-$LIFERAY_VERSION-jsf-$JSF_VERSION/deploy/test-setup-hook* ~/Portals/liferay.com/${LIFERAY_VERSION:0:3}/deploy/
 	echo "Moving ~/Portals/liferay.com/liferay-portal-$LIFERAY_VERSION-jsf-$JSF_VERSION/deploy/test-setup-hook* to ~/Portals/liferay.com/${LIFERAY_VERSION:0:3}/deploy/"
 fi
