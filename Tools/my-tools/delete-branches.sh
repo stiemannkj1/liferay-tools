4ebd84acf9dc59f67daa7984176ae3a507231637
Kyle Stiemann <stiemannkj1@gmail.com>
2015-05-28 11:57:24 -0400

    In delete-branches.sh, require 1 arg to run.

--- Tools/my-tools/delete-branches.sh
+++ Tools/my-tools/delete-branches.sh
@@ -1,5 +1,10 @@
 #!/opt/local/bin/bash
 
+if [ -z "$1" ]; then
+	echo "No branches specified for deletion. Exiting."
+	exit 0
+fi
+
 for branch in $(git branch | grep "$1"); do
 	git push --delete origin "$branch" && git branch -D "$branch"
 done
