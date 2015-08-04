d531b28a487507bc225260610ee8f537171a9e4b
Kyle Stiemann <stiemannkj1@gmail.com>
2015-08-04 15:54:44 -0400

    Changed vlddoc.sh to refer to vdldoc 2.0.

--- Tools/vdldoc/bin/vdldoc.sh
+++ Tools/vdldoc/bin/vdldoc.sh
@@ -1,3 +1,3 @@
 #!/opt/local/bin/bash
 
-java -jar .m2/repository/org/omnifaces/vdldoc/2.0-SNAPSHOT/vdldoc-2.0-SNAPSHOT.jar $@
+java -jar /Users/kylestiemann/.m2/repository/org/omnifaces/vdldoc/2.0/vdldoc-2.0.jar $@
