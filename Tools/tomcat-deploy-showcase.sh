c9eb9c4a6c520df53c63e4bd5737671270c51986
Kyle Stiemann <stiemannkj1@gmail.com>
2015-10-20 09:22:05 -0400

    Updated tomcat-deploy-showcase.sh for use with new repos.

--- Tools/tomcat-deploy-showcase.sh
+++ Tools/tomcat-deploy-showcase.sh
@@ -4,24 +4,21 @@ LAST_RUN_TIME_STAMP_FILE=~/Tools/.tomcat-deploy.sh.lastrun
 TOMCAT_WEBAPPS=~/Servers/apache-tomcat-7.0.56/webapps
 
 echo "Removing showcase files from Tomcat."
-rm -rf "$TOMCAT_WEBAPPS"/showcase-webapp-4.2.0-*
 
-if [[ "$@" == *u* || $(find "$LIFERAY_FACES/util" -mnewer "$LAST_RUN_TIME_STAMP_FILE" |  head -n 1) ]]
-then
-	mvn install -f "$LIFERAY_FACES/util/pom.xml"
+VERSION=$(xml-strings ~/Projects/liferay.com/liferay-faces-alloy/pom.xml :/project/version)
+
+rm -rf "$TOMCAT_WEBAPPS"/showcase-webapp-$VERSION-*
+
+if [[ $(gfind "./" -newer "$LAST_RUN_TIME_STAMP_FILE" | head -n 1) ]]; then
+	mvn clean install
 fi
 
-if [[ "$@" == *a* || $(find "$LIFERAY_FACES/alloy" -mnewer "$LAST_RUN_TIME_STAMP_FILE" | head -n 1) ]]
-then
-	mvn install -f "$LIFERAY_FACES/alloy/pom.xml"
-fi
-
-SHOWCASE_WEBAPP=$LIFERAY_FACES/demos/showcase/showcase-webapp
+SHOWCASE_WEBAPP=./demos/showcase/showcase-webapp
 
 cd "$SHOWCASE_WEBAPP"
-mvn install -P development
+mvn clean install -P "$1"
 echo "Copying showcase.war to Tomcat."
-cp "$SHOWCASE_WEBAPP"/target/showcase-webapp-*-SNAPSHOT.war "$TOMCAT_WEBAPPS/"
+cp ./target/showcase-webapp-*-SNAPSHOT.war "$TOMCAT_WEBAPPS/"
 touch "$LAST_RUN_TIME_STAMP_FILE"
 echo "Done."
 
