aa9915198b392c108db4ce37f2cb3594e6134efb
Kyle Stiemann <stiemannkj1@gmail.com>
2015-11-04 15:49:28 -0500

    Refactored liferay-deploy.sh to work with new repos.

--- Tools/liferay-deploy.sh
+++ Tools/liferay-deploy.sh
@@ -1,24 +1,21 @@
 #!/opt/local/bin/bash
 
 PORTALS_HOME="$HOME/Portals"
-LIFERAY_VERSION=NULL
+LIFERAY_VERSION=$(xml-strings ./pom.xml :/project/properties/liferay.version)
 
-if [[ $* =~ ee ]]; then
-	LIFERAY_VERSION=$(xml-strings $LIFERAY_FACES/pom.xml :/project/profiles/profile/properties/liferay.deploy.version)
-else
-	LIFERAY_VERSION=$(xml-strings $LIFERAY_FACES/pom.xml :/project/properties/liferay.version)
+JSF_VERSION=$(xml-strings ./pom.xml :/project/properties/faces.spec.version)
+
+if [[ $@ == *"jsf22"* ]]; then
+	JSF_VERSION=2.2
 fi
 
-JSF_VERSION=$(xml-strings $LIFERAY_FACES/pom.xml :/project/properties/faces.spec.version)
+for portlet in $(gfind "./demos" "./issues" -maxdepth 2 -name *-portlet | egrep "$1"); do
 
-for portlet in $(gfind "$LIFERAY_FACES/demos" "$LIFERAY_FACES/issues" -maxdepth 2 -name *-portlet | egrep "$1"); do
-
-	if [[ $portlet =~ icefaces3-chat-portlet ]]; then
-		echo "Skipping $portlet."
-	elif [ -e "$portlet/pom.xml" ]; then
-		cd $portlet && deploy.sh "${@:2}" development
+	if [ -e "$portlet/pom.xml" ]; then
+		pushd $portlet && deploy.sh "${@:2}" development
 		mv ~/Portals/liferay.com/liferay-portal-$LIFERAY_VERSION-jsf-$JSF_VERSION/deploy/${portlet##*/}* ~/Portals/liferay.com/${LIFERAY_VERSION:0:3}/deploy/
 		echo "Moving ~/Portals/liferay.com/liferay-portal-$LIFERAY_VERSION-jsf-$JSF_VERSION/deploy/${portlet##*/}* to ~/Portals/liferay.com/${LIFERAY_VERSION:0:3}/deploy/"
+		popd
 	else
 		echo "$portlet missing pom.xml. Ignoring."
 	fi
@@ -26,14 +23,16 @@ done
 
 if [[ tck =~ $1 ]]; then
 
-	cd $LIFERAY_FACES/test/bridge-tck/bridge-tck-main-portlet && deploy.sh "${@:2}" development
+	pushd  ~/Projects/liferay.com/liferay-faces-bridge-impl/test/bridge-tck/bridge-tck-main-portlet && deploy.sh "${@:2}" development
 	mv ~/Portals/liferay.com/liferay-portal-$LIFERAY_VERSION-jsf-$JSF_VERSION/deploy/bridge-tck-main-portlet* ~/Portals/liferay.com/${LIFERAY_VERSION:0:3}/deploy/
 	echo "Moving ~/Portals/liferay.com/liferay-portal-$LIFERAY_VERSION-jsf-$JSF_VERSION/bridge-tck-main-portlet* to ~/Portals/liferay.com/${LIFERAY_VERSION:0:3}/deploy/"
+	popd
 fi
 
 if [[ test-setup-hook =~ $1 ]]; then
 
-	cd $LIFERAY_FACES/test/test-setup-hook && deploy.sh "${@:2}" development
+	pushd ~/Projects/liferay.com/liferay-faces-portal/test/test-setup-hook && deploy.sh "${@:2}" development
 	mv ~/Portals/liferay.com/liferay-portal-$LIFERAY_VERSION-jsf-$JSF_VERSION/deploy/test-setup-hook* ~/Portals/liferay.com/${LIFERAY_VERSION:0:3}/deploy/
 	echo "Moving ~/Portals/liferay.com/liferay-portal-$LIFERAY_VERSION-jsf-$JSF_VERSION/deploy/test-setup-hook* to ~/Portals/liferay.com/${LIFERAY_VERSION:0:3}/deploy/"
+	popd
 fi
