1fe656931880716261193235ec63d1f45c143726
Kyle Stiemann <stiemannkj1@gmail.com>
2015-12-23 12:06:01 -0500

    Avoid searching for files in .git and target directories in fff.sh.

--- Tools/my-tools/fff.sh
+++ Tools/my-tools/fff.sh
@@ -1,13 +1,16 @@
 #!/opt/local/bin/bash
 
-if [ -z "$1" ]; then
-	echo "An argument is required."
-fi
-
 SEARCH_DIR="$2"
 
-if [ -z "SEARCH_DIR" ]; then
-	SEARCH_DIR="./"
+if [ -z "$SEARCH_DIR" ]; then
+	SEARCH_DIR="."
 fi
 
-gfind "$SEARCH_DIR" -regextype posix-extended -regex ".*/?$1[^/]*"
+if [ -z "$1" ]; then
+	gfind "$SEARCH_DIR" -not \( -path '*/.git' -prune \) -not \( -path '*/target' -prune \)
+else
+	gfind "$SEARCH_DIR" -not \( -path '*/.git' -prune \) -not \( -path '*/target' -prune \) -regextype posix-extended -regex ".*/?$1[^/]*"
+fi
+
+# TODO read ignores from gitignore:
+# gfind ./ -not \( -path '*/.git' -prune \) $(while read line; do printf " -not ( -path '*/$line' -prune ) "; done < .gitignore)
