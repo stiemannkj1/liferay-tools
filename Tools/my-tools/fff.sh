5ad58ada2523aec96286c3138951217467b28368
Kyle Stiemann <stiemannkj1@gmail.com>
2016-01-19 16:38:25 -0500

    In fff.sh, added feature to search multiple directories.

--- Tools/my-tools/fff.sh
+++ Tools/my-tools/fff.sh
@@ -1,15 +1,21 @@
 #!/opt/local/bin/bash
 
-SEARCH_DIR="$2"
+for ARG in "$@"; do
+	if [ -z "$SEARCH" ]; then
+		SEARCH="$ARG"
+	else
+		SEARCH_DIR+=("$ARG")
+	fi
+done
 
-if [ -z "$SEARCH_DIR" ]; then
-	SEARCH_DIR="."
+if [ ${#SEARCH_DIR[@]} -eq 0 ]; then
+	SEARCH_DIR+=(".")
 fi
 
 if [ -z "$1" ]; then
-	gfind "$SEARCH_DIR" -not \( -path '*/.git' -prune \) -not \( -path '*/target' -prune \)
+	gfind "${SEARCH_DIR[@]}" -not \( -path '*/.git' -prune \) -not \( -path '*/target' -prune \)
 else
-	gfind "$SEARCH_DIR" -not \( -path '*/.git' -prune \) -not \( -path '*/target' -prune \) -regextype posix-extended -regex ".*/?$1[^/]*"
+	gfind "${SEARCH_DIR[@]}" -not \( -path '*/.git' -prune \) -not \( -path '*/target' -prune \) -regextype posix-extended -regex ".*/?$1[^/]*"
 fi
 
 # TODO read ignores from gitignore:
