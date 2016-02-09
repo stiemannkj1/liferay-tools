43d0fa881acece877aac9ac22a467054030dacfd
Kyle Stiemann <stiemannkj1@gmail.com>
2016-02-09 10:55:23 -0500

    In rec-rep.sh, added feature to delete lines and added error messages.

--- Tools/my-tools/rec-rep.sh
+++ Tools/my-tools/rec-rep.sh
@@ -7,6 +7,8 @@ for ARG in "$@"; do
 	elif [ "$ARG" == '-0' ] || [ "$ARG" == '--multiline' ]; then
 		# ag does multiline searches by default
 		PERL_MULTILINE='-0'
+	elif [ "$ARG" == '-d' ] || [ "$ARG" == '--delete-matching-lines' ]; then
+		DELETE_MATCHING_LINES="true"
 	elif [ "$ARG" == '-n' ] || [ "$ARG" == '--file-names' ]; then
 		REC_REP_FILE_NAMES="true"
 	elif [ -z "$SEARCH" ]; then
@@ -19,8 +21,19 @@ for ARG in "$@"; do
 done
 
 if [ -z "$SEARCH" ]; then
-	echo 'You must specify a search regex or literal.'
+	echo 'Error: No search regex or literal specified. Exiting.'
 	exit 1
+fi
+
+if [ -n "$DELETE_MATCHING_LINES" ] && [ -n "$REC_REP_FILE_NAMES" ]; then
+	echo 'Error: "-d" (or "--delete-matching-lines") option is incompatible with "-n" (or "--file-names") option.'
+	echo 'Cannot delete matching lines while searching/replacing filenames. Exiting.'
+	exit 1
+fi
+
+if [ -n "$DELETE_MATCHING_LINES" ] && [ -n "$PERL_MULTILINE" ]; then
+	echo 'Warning: Ignoring multiline option "-0" (or "--multiline").'
+	echo 'Cannot use multiline matching while deleting lines.'
 fi
 
 if [ ${#SEARCH_DIR[@]} -eq 0 ]; then
@@ -31,8 +44,12 @@ FILES=$(ag --files-with-matches $AG_LITERAL "$SEARCH" "${SEARCH_DIR[@]}")
 
 if [ -n "$FILES" ]; then
 	SEARCH="${SEARCH//,/\\,}"
-	REPLACE="${REPLACE//,/\\,}"
-	perl $PERL_MULTILINE -p -i -e "s,$PERL_LITERAL$SEARCH,$REPLACE,g" $FILES
+	if [ -n "$DELETE_MATCHING_LINES" ]; then
+		perl -n -i -e "print unless m,$PERL_LITERAL$SEARCH," $FILES
+	else
+		REPLACE="${REPLACE//,/\\,}"
+		perl $PERL_MULTILINE -p -i -e "s,$PERL_LITERAL$SEARCH,$REPLACE,g" $FILES
+	fi
 	echo "$FILES"
 fi
 
