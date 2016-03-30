e1fe1d9fa418e90212a2ec5e4d7a64405e37b19e
Kyle Stiemann <stiemannkj1@gmail.com>
2016-03-30 16:26:58 -0400

    Added more permgen for Liferay.

--- Portals/liferay.com/properties/setenv.sh
+++ Portals/liferay.com/properties/setenv.sh
@@ -1,3 +1,3 @@
 # This file is symlinked in every portal/ folder using the following command:
 # for folder in [0-9].[0-9]?(-ee); do (cd $folder/tomcat-*/bin && ln -s /Users/kylestiemann/Portals/liferay.com/properties/setenv.sh setenv.sh); done
-JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true  -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx2048m -XX:MaxPermSize=512m -Djava.awt.headless=true"
+JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true  -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx2048m -XX:MaxPermSize=1024m -Djava.awt.headless=true"
