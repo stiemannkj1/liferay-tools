c1b64660a4b747b120b6b045f1c29099a254db8b
Kyle Stiemann <stiemannkj1@gmail.com>
2016-09-12 12:33:11 -0400

    Upgrade to vdldoc-2.1

--- Tools/vdldoc/bin/vdldoc.sh
+++ Tools/vdldoc/bin/vdldoc.sh
@@ -1,3 +1,3 @@
 #!/opt/local/bin/bash
 
-java -jar /Users/kylestiemann/.m2/repository/org/omnifaces/vdldoc/2.0/vdldoc-2.0.jar $@
+java -jar /Users/kylestiemann/.m2/repository/org/omnifaces/vdldoc/2.1/vdldoc-2.1.jar $@
