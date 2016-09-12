343d3e2582f41d09db5f1fb2abe9f2987cce152e
Kyle Stiemann <stiemannkj1@gmail.com>
2016-09-12 12:28:33 -0400

    Ensure that liferay-deploy.sh searches all appropriate dirs

--- Tools/liferay-deploy.sh
+++ Tools/liferay-deploy.sh
@@ -1,6 +1,37 @@
 #!/opt/local/bin/bash
 
-for portlet in $(gfind "./demo" "./demos" "./issue" "./issues" -maxdepth 2 -name *-portlet | egrep "$1"); do
+DIRS_TO_SEARCH=()
+
+if [ -d "./demo" ]; then
+	DIRS_TO_SEARCH+=("./demo")
+fi
+
+if [ -d "./demos" ]; then
+	DIRS_TO_SEARCH+=("./demos")
+fi
+
+if [ -d "./issue" ]; then
+	DIRS_TO_SEARCH+=("./issue")
+fi
+
+if [ -d "./issues" ]; then
+	DIRS_TO_SEARCH+=("./issues")
+fi
+
+if [ -d "./bridge" ]; then
+	DIRS_TO_SEARCH+=("./bridge")
+fi
+
+if [ -d "./bridge-ext" ]; then
+	DIRS_TO_SEARCH+=("./bridge-ext")
+fi
+
+if [ ${#DIRS_TO_SEARCH[@]} -eq 0 ]; then
+	echo "Error: no demos to deploy."
+	exit 1
+fi
+
+for portlet in $(gfind "${DIRS_TO_SEARCH[@]}" -maxdepth 2 -name *-portlet | egrep "$1"); do
 
 	if [ -e "$portlet/pom.xml" ]; then
 		(cd $portlet && deploy.sh "${@:2}" development)
