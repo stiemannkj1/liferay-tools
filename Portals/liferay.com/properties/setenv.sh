d75791719087b723025e59e25ab62c2a14ec9ef3
Kyle Stiemann <stiemannkj1@gmail.com>
2016-09-12 12:27:23 -0400

    Fix comments and add memory for setenv.sh.

--- Portals/liferay.com/properties/setenv.sh
+++ Portals/liferay.com/properties/setenv.sh
@@ -1,3 +1,3 @@
 # This file is symlinked in every portal/ folder using the following command:
-# for folder in [0-9].[0-9]?(-ee); do (cd $folder/tomcat-*/bin && ln -s /Users/kylestiemann/Portals/liferay.com/properties/setenv.sh setenv.sh); done
-JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true  -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx2048m -XX:MaxPermSize=1024m -Djava.awt.headless=true"
+# for folder in [0-9].[0-9]?(-ee); do (cd $folder/tomcat-*/bin && rm setenv.sh; ln -s /Users/kylestiemann/Portals/liferay.com/properties/setenv.sh setenv.sh); done
+JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true  -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx3072m -XX:MaxPermSize=1536m -Djava.awt.headless=true"
