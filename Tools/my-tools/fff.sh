bc666568cac82a82b10784f4a1d788202081e8f5
Kyle Stiemann <stiemannkj1@gmail.com>
2016-09-12 12:29:48 -0400

    Add option to search all dirs with fff.sh

--- Tools/my-tools/fff.sh
+++ Tools/my-tools/fff.sh
@@ -1,7 +1,9 @@
 #!/opt/local/bin/bash
 
 for ARG in "$@"; do
-	if [ -z "$SEARCH" ]; then
+	if [ "$ARG" == '-a' ] || [ "$ARG" == '--all' ]; then
+		ALL="true"
+	elif [ -z "$SEARCH" ]; then
 		SEARCH="$ARG"
 	else
 		SEARCH_DIR+=("$ARG")
@@ -12,11 +14,17 @@ if [ ${#SEARCH_DIR[@]} -eq 0 ]; then
 	SEARCH_DIR+=(".")
 fi
 
-if [ -z "$1" ]; then
-	gfind "${SEARCH_DIR[@]}" -not \( -path '*/.git' -prune \) -not \( -path '*/target' -prune \)
-else
-	gfind "${SEARCH_DIR[@]}" -not \( -path '*/.git' -prune \) -not \( -path '*/target' -prune \) -regextype posix-extended -regex ".*/?$1[^/]*"
+FIND_COMMAND='gfind "${SEARCH_DIR[@]}"'
+
+if [ -z "$ALL" ]; then
+	FIND_COMMAND+=' -not \( -path '"'"'*/.git'"'"' -prune \) -not \( -path '"'"'*/target'"'"' -prune \)'
 fi
+
+if [ -n "$1" ]; then
+	FIND_COMMAND+=' -regextype posix-extended -regex ".*/?$1[^/]*"'
+fi
+
+eval $FIND_COMMAND
 
 # TODO read ignores from gitignore:
 # gfind ./ -not \( -path '*/.git' -prune \) $(while read line; do printf " -not ( -path '*/$line' -prune ) "; done < .gitignore)
