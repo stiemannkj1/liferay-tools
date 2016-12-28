409a3e931442e970ebcf3dcbba51db58fb7acd16
Kyle Stiemann <stiemannkj1@gmail.com>
2016-12-28 09:08:19 -0500

    In markdown converters, move duplicate JS to markdown-converter-util.js

---
+++ Tools/markdown-converter-util.js
@@ -0,0 +1,76 @@
+function MarkdownConverter(markdownRenderer) {
+
+    marked.setOptions({
+        renderer: markdownRenderer,
+        gfm: true,
+        tables: true,
+        breaks: true,
+        pedantic: false,
+        sanitize: false,
+        smartLists: true,
+        smartypants: false
+    });
+
+    this.convert = function(markdownTextarea, convertedTextarea) {
+        var markdownText = markdownTextarea.value;
+        var convertedText = marked(markdownText);
+
+        if (convertedText.charAt(0) === '\n') {
+            convertedText = convertedText.substring(1);
+        }
+
+        convertedTextarea.value = convertedText;
+    };
+};
+
+var Util = new (function() {
+
+    function indentOrUnindentSelectedLines(indent, markdownTextarea) {
+        var selectionStart = markdownTextarea.selectionStart;
+        var selectionEnd = markdownTextarea.selectionEnd;
+        var firstLineIndex = markdownTextarea.value.substring(0, selectionStart).lastIndexOf('\n') + 1;
+
+        if (firstLineIndex === -1) {
+            firstLineIndex = 0;
+        }
+
+        var stringBeforeSelection = markdownTextarea.value.substring(0, firstLineIndex);
+        var stringAfterSelection = markdownTextarea.value.substring(selectionEnd);
+        var changedLines;
+        var newSelectionIndex;
+
+        if (indent) {
+            newSelectionIndex = selectionStart + 4;
+            changedLines = '    ' + markdownTextarea.value.substring(firstLineIndex, selectionEnd)
+                    .replace(/\n/g, '\n    ');
+        } else {
+            var textToUnindent = markdownTextarea.value.substring(firstLineIndex, markdownTextarea.selectionEnd);
+            newSelectionIndex = selectionStart + (textToUnindent.startsWith('    ') ? -4 : 0);
+            changedLines = textToUnindent.replace(/^    /, '')
+                    .replace(/\n    /g, '\n');
+        }
+
+        markdownTextarea.value = stringBeforeSelection + changedLines + stringAfterSelection;
+        markdownTextarea.focus();
+        markdownTextarea.selectionStart = newSelectionIndex;
+        markdownTextarea.selectionEnd = markdownTextarea.value.lastIndexOf(stringAfterSelection);
+    }
+
+    this.indentSelectedLines = function(markdownTextarea) {
+        indentOrUnindentSelectedLines(true, markdownTextarea);
+    };
+
+    this.unindentSelectedLines = function(markdownTextarea) {
+        indentOrUnindentSelectedLines(false, markdownTextarea);
+    };
+
+    this.unescapeXML = function(text) {
+        // split and join are used instead of replace since replace does not replace all occurences. For more
+        // details see: http://stackoverflow.com/questions/1144783/how-to-replace-all-occurrences-of-a-string-in-javascript#1145525
+        return text.split('&lt;').join('<')
+                .split('&gt;').join('>')
+                .split('&quot;').join('"')
+                .split('&#39;').join("'")
+                .split('&amp;').join('&');
+    };
+})();
