4d57501391f0db0c924ae5372cf7bdb90ca5c2f6
Kyle Stiemann <stiemannkj1@gmail.com>
2018-04-17 18:01:41 -0400

    Push before sending PRs

--- Tools/my-tools/hub-pull-request.sh
+++ Tools/my-tools/hub-pull-request.sh
@@ -16,6 +16,7 @@
 #
 ################################################################################
 
+git push origin HEAD
 export GIT_EDITOR=no-edit.sh
 hub pull-request "$@" | tee /dev/tty | ag --nocolor '^http' | copy.sh
 export GIT_EDITOR=
